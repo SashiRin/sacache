dc22660c12dc1c78df38c7369916d852fe5d4d78
SashiRin <ldy.nero@gmail.com>
2021-03-02 14:38:35 +0900

    Add sacache_test

---
+++ sacache_test.go
@@ -0,0 +1,33 @@
+package sacache
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCache(t *testing.T) {
+	table := NewSaCache("hello")
+	var (
+		key = 11
+		val = 233
+		ttl = time.Duration(20)
+		it1 = NewCacheItem(val, ttl)
+	)
+	// Set
+	table.Set(key, it1)
+	// Get
+	v, ok := table.Get(key)
+	if !ok {
+		t.Errorf("key: %v not found in cache!", key)
+	}
+
+	if v.Value() != val {
+		t.Errorf("val = %v; expected %v", v.Value(), val)
+	}
+	// Delete
+	table.Delete(key)
+	v, ok = table.Get(key)
+	if ok {
+		t.Errorf("key: %v not deleted in cache!", key)
+	}
+}
