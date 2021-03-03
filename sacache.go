61e814e4dacc95f7d9a67fe05020edafdbff0e78
SashiRin <ldy.nero@gmail.com>
2021-03-03 21:30:25 +0900

    Add lock for cache operations

--- sacache.go
+++ sacache.go
@@ -1,5 +1,7 @@
 package sacache
 
+import "sync"
+
 // SaCache is a in-memory cache service.
 type SaCache struct {
 	// The name of table.
@@ -7,6 +9,9 @@ type SaCache struct {
 
 	// The map instance of table.
 	items map[interface{}]*CacheItem
+
+	// Lock
+	lock sync.RWMutex
 }
 
 // NewSaCache returns the pointer of a newly created SaCache instance.
@@ -20,17 +25,23 @@ func NewSaCache(name string) *SaCache {
 
 // Get returns the CacheItem pointer of given key.
 func (c *SaCache) Get(key interface{}) (*CacheItem, bool) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
 	v, ok := c.items[key]
 	return v, ok
 }
 
 // Set add new k-v pair in the cache.
 func (c *SaCache) Set(key interface{}, val *CacheItem) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 	c.items[key] = val
 }
 
 // Delete deletes value given key.
 func (c *SaCache) Delete(key interface{}) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
 	_, ok := c.items[key]
 	if !ok {
 		return
