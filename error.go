662fbbdc9d58dca98f9bc9fac1a0c7bac8e5e14a
SashiRin <ldy.nero@gmail.com>
2021-03-13 23:31:18 +0900

    Make error strings not capitalized

--- error.go
+++ error.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	// ErrNotFound means given key is not found in the cache.
-	ErrNotFound = errors.New("Given key not found")
+	ErrNotFound = errors.New("given key not found")
 	// ErrExpired means given item is expired.
-	ErrExpired = errors.New("Item expired")
+	ErrExpired = errors.New("item expired")
 )
